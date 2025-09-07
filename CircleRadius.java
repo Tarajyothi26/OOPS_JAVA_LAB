d6194c0e361a0f03e56b8270373af73c2eff61e9
Tarajyothi26 <24wh1a05v3@bvrithyderabad.edu.in>
2025-09-07 21:50:10 +0530

    Create CircleRadius.java

---
+++ CircleRadius.java
@@ -0,0 +1,11 @@
+public class CircleRadius {
+    public static void main(String[] args) {
+        double radius = 7;
+        double area = Math.PI * radius * radius;
+        double circumference = 2 * Math.PI * radius;
+
+        System.out.println("Circle radius: " + radius);
+        System.out.println("Area: " + area);
+        System.out.println("Circumference: " + circumference);
+    }
+}
